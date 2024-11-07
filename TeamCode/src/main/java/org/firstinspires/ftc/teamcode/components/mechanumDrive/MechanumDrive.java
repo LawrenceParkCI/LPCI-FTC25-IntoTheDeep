e2f77cba1feeb5d997d7818b214e177e05eff6f8
ConnorFeeney <connor.w.feeney@gmail.com>
2024-11-07 18:03:18 -0500

    Added field centric movement
    Fixed some errors and added the option for the robot to move in a field centric motion, untested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/mechanumDrive/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/mechanumDrive/MechanumDrive.java
@@ -1,47 +1,144 @@
 package org.firstinspires.ftc.teamcode.components.mechanumDrive;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 /**
  * FTC Into The Deep 24-25 <br>
  * A class for using a mechanum drive train,
- * This class does not support dead wheel odometry
+ * This class does not support dead wheel odometry.
  * <br><br>
  * Last Updated: November 7th, 2024
  * @author Connor Feeney
  */
 public class MechanumDrive {
-    DcMotor leftFrontMotor;
-    DcMotor leftBackMotor;
-    DcMotor rightFrontMotor;
-    DcMotor rightBackMotor;
+    @FunctionalInterface
+    private interface Action{
+        void execute(float lY, float lX, float rX);
+    }
+
+    private Action driveAction = this::driveRobotCentric;
+    private boolean fieldCentric = false;
+
+    private final IMU imu;
+
+    private final DcMotor leftFrontMotor;
+    private final DcMotor leftBackMotor;
+    private final DcMotor rightFrontMotor;
+    private final DcMotor rightBackMotor;
 
     /**
-     * MechanumDrive Constructor, All motors should have a positive forward power
+     * MechanumDrive Constructor, All motors should have a positive forward power.
      * @param leftFrontMotor Your robots left front motor
      * @param leftBackMotor Your robots left back motor
      * @param rightFrontMotor Your robots right front motor
      * @param rightBackMotor Your robots right back motor
      */
-    public MechanumDrive(DcMotor leftFrontMotor, DcMotor leftBackMotor, DcMotor rightFrontMotor, DcMotor rightBackMotor){
+    public MechanumDrive(DcMotor leftFrontMotor, DcMotor leftBackMotor, DcMotor rightFrontMotor, DcMotor rightBackMotor, HardwareMap hardwareMap){
         //Store pointer to motor objects
         this.leftFrontMotor = leftFrontMotor;
         this.leftBackMotor = leftBackMotor;
         this.rightFrontMotor = rightFrontMotor;
         this.rightBackMotor = rightBackMotor;
+
+        //Get the imu pointer
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.resetYaw();
     }
 
     /**
-     * Drive the robot with according values
+     * Set the robots imu / rev hub orientation, this should be set before using field centric movement.
+     * This method will also reset the yaw of your imu for consistency.
+     * @param logoFacingDirection The direction the logo is facing
+     * @param usbFacingDirection The direction the usb port is facing
+     * @see RevHubOrientationOnRobot
+     * @see RevHubOrientationOnRobot.LogoFacingDirection
+     * @see RevHubOrientationOnRobot.UsbFacingDirection
+     */
+    public void setImuOrientation(RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection, RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection){
+        //Create an orientation based on params
+        RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(logoFacingDirection, usbFacingDirection);
+
+        //Set orientation
+        imu.initialize(new IMU.Parameters(orientation));
+        imu.resetYaw();
+    }
+
+    /**
+     * Set if the mechanum drive should follow a field centric movement style or not
+     * Some drivers may prefer filed centric movement as it can make several evasive
+     * maneuvers easier while still translating the robot in a given direction.
+     * By default the mechanum drive moves in a robot centric movement style.
+     * Make sure to properly set the rev hub orientation with {@link #setImuOrientation(RevHubOrientationOnRobot.LogoFacingDirection, RevHubOrientationOnRobot.UsbFacingDirection) setImuOrientation(logoFacingDirection, usbFacingDirection)}
+     * @param fieldCentric True to make the robot drive field centric, False to make it robot centric
+     * @see #setImuOrientation(RevHubOrientationOnRobot.LogoFacingDirection, RevHubOrientationOnRobot.UsbFacingDirection) setImuOrientation(logoFacingDirection, usbFacingDirection)
+     */
+    public void setFieldCentric(boolean fieldCentric){
+        driveAction = fieldCentric ? this::driveFieldCentric : this::driveRobotCentric;
+        this.fieldCentric = fieldCentric;
+    }
+
+    /**
+     * Check if the robot is set to follow a field centric movement style.
+     * @return True if the robot drives field centric
+     */
+    public boolean getFieldCentric(){
+        return fieldCentric;
+    }
+
+    /**
+     * Drive the robot with according values.
      * @param lY Left stick y power
      * @param lX Left stick x power
-     * @param rY Right stick y power
+     * @param rX Right stick y power
      */
-    public void drive(float lY, float lX, float rY){
+    public void drive(float lY, float lX, float rX){
+        driveAction.execute(lY, lX, rX); //Calls to whatever action is set based on setFieldCentric(Boolean fieldCentric)
+    }
+
+    /**
+     * Drive the robot in a robot centric motion based on motion vectors.
+     * @param lY Y-Component of the translation vector
+     * @param lX X-Component of the translation vector
+     * @param rX Rotation vector
+     */
+    private void driveRobotCentric(float lY, float lX, float rX){
+        //Calculate denominator
+        double denominator = Math.max(Math.abs(lY) + Math.abs(lX) + Math.abs(rX), 1);
+
         //Set all motor powers
-        leftFrontMotor.setPower(-lY + lX + rY);
-        leftBackMotor.setPower(-lY - lX + rY);
-        rightFrontMotor.setPower(-lY - lX - rY);
-        rightBackMotor.setPower(-lY + lX - rY);
+        leftFrontMotor.setPower((-lY + lX + rX) / denominator);
+        leftBackMotor.setPower((-lY - lX + rX) / denominator);
+        rightFrontMotor.setPower((-lY - lX - rX) / denominator);
+        rightBackMotor.setPower((-lY + lX - rX) / denominator);
+    }
+
+    /**
+     * Drive the robot in a field centric motion based on motion vectors.
+     * @param lY Y-Component of the translation vector
+     * @param lX X-Component of the translation vector
+     * @param rX Rotation vector
+     */
+    private void driveFieldCentric(float lY, float lX, float rX){
+        //Get robots current heading
+        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        //Rotate motion vectors
+        double y = -lY;
+        double rotX = (double) lX * Math.cos(-heading) - y * Math.sin(-heading);
+        double rotY = (double) lX * Math.sin(-heading) - y * Math.cos(-heading);
+
+        //Calculate denominator
+        double denominator = Math.max(Math.abs(rotX) + Math.abs(rotY) + Math.abs(rX), 1);
+
+        //Set all motor powers
+        leftFrontMotor.setPower((rotY + rotX + rX) / denominator);
+        leftBackMotor.setPower((rotY - rotX + rX) / denominator);
+        rightFrontMotor.setPower((rotY - rotX - rX) / denominator);
+        rightBackMotor.setPower((rotY + rotX - rX) / denominator);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.components.mechanumDrive.MechanumDrive;
 
@@ -12,6 +13,7 @@ public class DriveTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        //Get all motor objects
         DcMotor leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
         DcMotor leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
         DcMotor rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
@@ -25,19 +27,35 @@ public class DriveTest extends LinearOpMode {
         rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         */
 
-        MechanumDrive d = new MechanumDrive(leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor);
+        MechanumDrive d = new MechanumDrive(leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor, hardwareMap);
+
+        Gamepad currGamepad = new Gamepad();
+        Gamepad prevGamepad = new Gamepad();
 
         telemetry.addData("Status: ", "Initialized");
         telemetry.update();
 
         waitForStart();
         while(opModeIsActive()){
+            //Record gamepad
+            prevGamepad.copy(currGamepad);
+            currGamepad.copy(gamepad1);
+
+            //Switch driving mode on gamepad a press
+            if(currGamepad.a && !prevGamepad.a){
+                d.setFieldCentric(!d.getFieldCentric());
+            }
+
+            //Drive the robot
             d.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_y);
 
+            //Update telemetry
             telemetry.addData("Status: ", "Active");
+            telemetry.addData("Drive Mode: ", d.getFieldCentric() ? "Field Centric" : "Robot Centric");
             telemetry.addData("lY: ", -gamepad1.left_stick_y);
             telemetry.addData("lX: ", gamepad1.left_stick_x);
             telemetry.addData("rY: ", gamepad1.right_stick_y);
+            telemetry.update();
         }
     }
 }
