c447646208252eceea0901f028d0b0f1ff47c410
ConnorFeeney <connor.w.feeney@gmail.com>
2025-01-06 09:12:49 -0500

    Added Intake Setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/lift/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/lift/Lift.java
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.teamcode.components.util.PID;
 
 public class Lift {
     private final LinearOpMode opMode;
-    private final DcMotor armMotor;
+    private final DcMotor liftMotor;
     private final DcMotor dumbMotor;
     
     private double maxEncoderValue;
@@ -18,12 +18,12 @@ public class Lift {
     PID pid = new PID(0,0,0);
     double kg = 0;
 
-    public Lift(DcMotor armMotor, DcMotor dumbMotor, double maxEncoderValue, LinearOpMode opMode) throws IllegalArgumentException{
+    public Lift(DcMotor liftMotor, DcMotor dumbMotor, double maxEncoderValue, LinearOpMode opMode) throws IllegalArgumentException{
         this.opMode = opMode;
 
-        this.armMotor = armMotor;
-        this.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        this.armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.liftMotor = liftMotor;
+        this.liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         this.dumbMotor = dumbMotor;
         this.dumbMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -44,10 +44,10 @@ public class Lift {
     }
 
     public void update(){
-        double error = targetHeight - (armMotor.getCurrentPosition() / maxEncoderValue);
+        double error = targetHeight - (liftMotor.getCurrentPosition() / maxEncoderValue);
         if(Math.abs(error) > Math.abs(tolerance)){
-            double power = pid.calculate(armMotor.getCurrentPosition() / maxEncoderValue, targetHeight) + kg;
-            armMotor.setPower(power);
+            double power = pid.calculate(liftMotor.getCurrentPosition() / maxEncoderValue, targetHeight) + kg;
+            liftMotor.setPower(power);
             dumbMotor.setPower(power);
         }
     }
@@ -65,13 +65,13 @@ public class Lift {
 
     public void setHeight(double height) throws IllegalArgumentException{
         if(height > 1 || height < 0){
-            throw new IllegalArgumentException("Height Must > 1 and < 0");
+            throw new IllegalArgumentException("Height Must <= 1 and >= 0");
         }
         targetHeight = height;
     }
 
     public void bufferTelemetry(){
-        opMode.telemetry.addData("Arm Position: ", armMotor.getCurrentPosition());
-        opMode.telemetry.addData("Arm Power: ", armMotor.getPower());
+        opMode.telemetry.addData("Arm Position: ", liftMotor.getCurrentPosition());
+        opMode.telemetry.addData("Arm Power: ", liftMotor.getPower());
     }
 }
